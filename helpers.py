1c2eb0ff05a7a96bebe2776f5dbf2f1b296dfbf7
jss77 <jss77@bath.ac.uk>
2023-03-28 16:32:52 +0100

    Adding helper file with useful functions. Refactoring

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from os import listdir
+
+
+def white_to_black(img):
+    for m in range(0, img.shape[0]):
+        for n in range(0, img.shape[1]):
+            if img[m, n] == 255:
+                img[m, n] = 0
+    return img
+
+
+def get_object_name(file):
+    object_name = file[4:len(file) - 4]
+    print(object_name)
